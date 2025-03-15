db50e94acca084c038e949bd8eb274beb72d2d8b
范勤阳 <fqymtu@gmail.com>
2025-03-15 14:45:30 +0800

    111

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+// import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
+import { CatsController } from './cats.controller';
+import { DevtoolsModule } from '@nestjs/devtools-integration';
+import { CatsService } from './cats.service';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    DevtoolsModule.register({
+      http: process.env.NODE_ENV !== 'production',
+      port: 8000, // 指定端口
+    }),
+  ],
+  controllers: [CatsController],
+  providers: [AppService, CatsService],
 })
 export class AppModule {}
---
+++ src/cats.controller.ts
@@ -0,0 +1,18 @@
+import { Body, Controller, Post, Get } from '@nestjs/common';
+import { CatsService } from './cats.service';
+import { Cat } from './interfaces/cat.interface';
+@Controller({ path: 'cat' })
+export class CatsController {
+  // 使用 Providers
+  constructor(private readonly catsService: CatsService) {}
+
+  @Post()
+  create(@Body('cat') cat: Cat) {
+    this.catsService.create(cat);
+  }
+
+  @Get()
+  findAll(): Cat[] {
+    return this.catsService.findAll();
+  }
+}
