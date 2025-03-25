c0572b9758cf22586822730b90e4d03551db7d7e
范勤阳 <fqymtu@gmail.com>
2025-03-25 17:54:26 +0800

    Middleware 中间件

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
-
+import { LoggerMiddleware } from './logger.middleware';
+import { CatsController } from './cats/cats.controller';
 @Module({
   imports: [CatsModule],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LoggerMiddleware).forRoutes(CatsController);
+  }
+}
