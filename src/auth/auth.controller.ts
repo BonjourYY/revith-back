9416341ef0a5f767dbeac3b2e6c2b2e0f19d100a
范勤阳 <fqymtu@gmail.com>
2025-04-11 18:48:55 +0800

    1212312

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -14,18 +14,19 @@ export class AuthController {
   }
 
   @Post('userinfo')
-  async getUserInfo(@Body() code: string) {
-    console.log(code);
-    const access_token_url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${process.env.APPID}&secret=${process.env.APPSECRET}&code=${code}&grant_type=authorization_code`;
-    const access_token_response: AxiosResponse<{
-      access_token: string;
-      openid: string;
-    }> = await axios.get(access_token_url);
-    const access_token = access_token_response.data.access_token;
-    const openid = access_token_response.data.openid;
-    const user_info_url = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`;
-    const user_info_response: AxiosResponse<Record<string, any>> =
-      await axios.get(user_info_url);
-    return user_info_response.data;
+  getUserInfo(@Body() code: string) {
+    return { a: 1111 };
+    // console.log(code);
+    // const access_token_url = `https://api.weixin.qq.com/sns/oauth2/access_token?appid=${process.env.APPID}&secret=${process.env.APPSECRET}&code=${code}&grant_type=authorization_code`;
+    // const access_token_response: AxiosResponse<{
+    //   access_token: string;
+    //   openid: string;
+    // }> = await axios.get(access_token_url);
+    // const access_token = access_token_response.data.access_token;
+    // const openid = access_token_response.data.openid;
+    // const user_info_url = `https://api.weixin.qq.com/sns/userinfo?access_token=${access_token}&openid=${openid}&lang=zh_CN`;
+    // const user_info_response: AxiosResponse<Record<string, any>> =
+    //   await axios.get(user_info_url);
+    // return user_info_response.data;
   }
 }
